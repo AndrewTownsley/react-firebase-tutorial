702ad0794845a39798b56c27ce6c6d1714c0dba6
Andrew TOwnsley <atownsley86@gmail.com>
2021-07-16 21:58:47 -0600

    initial file setup from the class exercise

---
+++ news-api-react/src/data.js
@@ -0,0 +1,10 @@
+const url = (`https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY`);
+
+export const news_categories = [
+    "business",
+    "sports",
+    "entertainment",
+    "health",
+    "science",
+    "technology",
+];
