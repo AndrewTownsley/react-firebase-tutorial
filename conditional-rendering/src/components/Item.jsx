c9f915a80b396088670755d00888554c79e6d966
Andrew TOwnsley <atownsley86@gmail.com>
2021-09-26 17:12:50 -0600

    consitional rendering in react

---
+++ conditional-rendering/src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Item = () => {
+    return (
+        <div className="item">
+            <h3>Item</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, quibusdam?
+            </p>
+        </div>
+    )
+}
+
+export default Item
---
+++ conditional-rendering/src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div>
+                    <ul className="header-list">
+                <li>Home</li>
+                <li>Aboute</li>
+                <li>Services</li>
+                <li>Products</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default List
